41d876a3a98de01de0c230c2ee92a48a74a3e09b
abelcip <abelcipriano97@gmail.com>
2024-05-27 21:17:59 -0300

    Home

---
+++ portfolio-react/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="contenido-header">
+        <img src={`${process.env.PUBLIC_URL}/abel.jpg`} alt="Foto de perfil" className="fotoperfil" />
+        <h1>Abel Cipriano</h1>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
